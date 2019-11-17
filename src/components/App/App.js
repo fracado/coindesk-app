e5609793f56f1fd368f3cea135c7292ef3269c76
fbath <phoenixa@gmx.de>
2019-11-17 14:15:24 +0100

    added start/end-date calculation functions to fetch query

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -12,8 +12,39 @@ class App extends Component {
   }
 
   componentDidMount() {
+    // calculate current date for fetch query
+    let currentDay = function(sp) {
+      let today = new Date();
+      var dd = today.getDate();
+      var mm = today.getMonth() + 1;
+      var yyyy = today.getFullYear();
+
+      if (dd < 10) dd = '0' + dd;
+      if (mm < 10) mm = '0' + mm;
+      return yyyy + sp + mm + sp + dd;
+    };
+    let enddate = currentDay('-');
+    console.log(enddate);
+
+    // calculate date 2 weeks ago for fetch query
+    let twoWeeksAgo = function(sp) {
+      let day = new Date(Date.now() - 12096e5);
+      var dd = day.getDate();
+      var mm = day.getMonth() + 1;
+      var yyyy = day.getFullYear();
+
+      if (dd < 10) dd = '0' + dd;
+      if (mm < 10) mm = '0' + mm;
+      return yyyy + sp + mm + sp + dd;
+    };
+    let startdate = twoWeeksAgo('-');
+    console.log(startdate);
+
     fetch(
-      'https://api.coindesk.com/v1/bpi/historical/close.json?start=2019-11-01&end=2019-11-15',
+      'https://api.coindesk.com/v1/bpi/historical/close.json?start=' +
+        startdate +
+        '&end=' +
+        enddate,
       // 'https://api.coindesk.com/v1/bpi/currentprice/EUR.json',
       {
         method: 'get',
@@ -37,9 +68,8 @@ class App extends Component {
     return (
       <div className='App'>
         <header className='App-header'>
+          {/* load Table component if data is not null */}
           {this.state.data !== null && <Table rates={this.state.data.bpi} />}
-          {/* <div>{this.state.data && Object.values(this.state.data.bpi)}</div> */}
-          {/* <div>{this.state.data && this.state.data.bpi.EUR.rate}</div> */}
         </header>
       </div>
     );
