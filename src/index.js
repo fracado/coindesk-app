a83401263e822bdc1191f00ad564dfc9d2252440
fbath <phoenixa@gmx.de>
2019-11-19 02:16:30 +0100

    minor updates

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './components/App/App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '@fortawesome/fontawesome-free/css/all.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
